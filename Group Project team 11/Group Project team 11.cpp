2b713cca69266ea35fa9c6065f530c37fe4573b3
Rina Furusaki <270073417@yoobeestudent.ac.nz>
2021-06-18 10:35:33 +1200

    group project

---
+++ Group Project team 11/Group Project team 11.cpp
@@ -0,0 +1,18 @@
+// Group Project team 11.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+	cout << 
+	
+	
+	
+	
+	
+	
+	return 0;
+}
